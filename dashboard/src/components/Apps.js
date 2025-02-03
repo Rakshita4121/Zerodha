a63edb318c38170879871e26d4638578ef40440f
Rakshita4121 <143410907+Rakshita4121@users.noreply.github.com>
2025-02-03 22:55:14 +0530

    created dashboard components

---
+++ dashboard/src/components/Apps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Apps = () => {
+  return <h1>Apps</h1>;
+};
+
+export default Apps;
+
+
+// mongodb+srv://singamrakshitareddy:rakshitaZerodha100@zerodhaclonecluster.grgxg.mongodb.net/?retryWrites=true&w=majority&appName=ZerodhaCloneCluster
